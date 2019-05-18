654314f47f27397a3430d4606ac0f80d1b8bcb69
dylanfisher <hi@dylanfisher.com>
2019-05-18 11:42:40 -0400

    Update gulpfile and assets directory

--- gulpfile.js
+++ gulpfile.js
@@ -2,101 +2,58 @@
 var gulp = require('gulp');
 
 // Include Plugins
-var jshint       = require('gulp-jshint');
 var sass         = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+var postcss      = require('gulp-postcss');
+var gulp         = require('gulp');
+var autoprefixer = require('autoprefixer');
+var cssnano      = require('cssnano');
 var concat       = require('gulp-concat');
 var uglify       = require('gulp-uglify');
 var rename       = require('gulp-rename');
-var connect      = require('gulp-connect');
+var livereload   = require('gulp-livereload');
 var notify       = require('gulp-notify');
-var del          = require('del');
-
-// Lint Task
-gulp.task('lint', function() {
-  return gulp.src([
-      'js/src/scripts/application.js',
-      'js/src/scripts/**/*.js'
-    ])
-    .pipe(jshint())
-    // Use gulp-notify as jshint reporter
-    .pipe(notify(function(file) {
-      if (file.jshint.success) {
-        // Don't show something if success
-        return false;
-      }
-
-      var errors = file.jshint.results.map(function(data) {
-        if (data.error) {
-          return "(" + data.error.line + ':' + data.error.character + ') ' + data.error.reason;
-        }
-      }).join("\n");
-      return file.relative + " (" + file.jshint.results.length + " errors)\n" + errors;
-    }))
-    .on('error', onError);
-});
 
 // Compile Sass
-gulp.task('sass', function() {
-  return gulp.src('css/sass/application.scss')
+gulp.task('css', function() {
+  return gulp.src('assets/stylesheets/style.scss')
     .pipe(sass({outputStyle: 'compressed'})).on('error', sass.logError)
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(rename('application.min.css'))
-    .pipe(gulp.dest('./css'))
-    .pipe(connect.reload())
+    .pipe(rename('style.css'))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(gulp.dest('./'))
+    .pipe(livereload())
     .on('error', onError);
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
   return gulp.src([
-      'js/src/vendor/*.js',
-      'js/src/vendor/**/*.js',
-      'js/src/application.js',
-      'js/src/scripts/*.js',
-      'js/src/scripts/**/*.js',
-    ], {
-      allowEmpty: true
-    })
+      'assets/javascripts/src/vendor/*.js',
+      'assets/javascripts/src/vendor/**/*.js',
+      'assets/javascripts/src/application.js',
+      'assets/javascripts/src/scripts/*.js',
+      'assets/javascripts/src/scripts/**/*.js',
+    ])
     .pipe(concat('application.js'))
-    .pipe(gulp.dest('js/dist'))
+    .pipe(gulp.dest('assets/javascripts/dist'))
     .pipe(rename('application.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('js/dist'))
-    .pipe(connect.reload())
+    .pipe(gulp.dest('assets/javascripts/dist'))
+    .pipe(livereload())
     .on('error', onError);
 });
 
-// Livereload and web server
-gulp.task('connect', function(done) {
-  connect.server({
-    livereload: true
-  });
-  done();
-});
-
 // Watch Files for Changes
-gulp.task('watch', function(done) {
-  gulp.watch('**/*.html', gulp.series(connect.reload));
-  gulp.watch('js/src/**/*.js', gulp.series('lint', 'scripts'));
-  gulp.watch('css/sass/**/*.scss', gulp.series('sass'));
-  done();
-});
-
-// Clean up build files
-gulp.task('clean', function () {
-  return del([
-    'css/build/**/*',
-    'js/build/**/*'
-  ]);
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('**/*.html').on('change', function(file) {
+    livereload.changed(file);
+  });
+  gulp.watch('assets/javascripts/src/**/*.js', gulp.series('scripts'));
+  gulp.watch('assets/stylesheets/**/*.scss', gulp.series('css'));
 });
 
 // Default Task
-// gulp.task('default', ['lint', 'sass', 'scripts', 'clean', 'watch']);
-gulp.task('default', gulp.series('lint', gulp.parallel('sass', 'scripts'), 'clean', 'connect', 'watch'));
+gulp.task('default', gulp.series(gulp.parallel('css', 'scripts'), 'watch'));
 
 // Catch errors
 function onError(err) {
